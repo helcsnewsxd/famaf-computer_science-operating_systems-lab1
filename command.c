6895ea16e6402b416aa7d71d2451910a6541609e
Juan Bratti <juanbratti@mi.unc.edu.ar>
2022-08-26 16:39:35 -0300

    scommand fully implemented w/o any fails in tests. Added comments & testing files.

--- command.c
+++ command.c
@@ -8,7 +8,7 @@
 
 #include "command.h"
 #include "strextra.h"
-// Needed library for queue defined structure (GQueue)
+// Needed library for the queue defined structure (GQueue)
 
 // ------------------------- SCOMMAND TAD -------------------------
 
@@ -23,6 +23,7 @@ struct scommand_s{
 // ----------- FUNCTIONS --------------
 
 scommand scommand_new(void){
+    // Allocating memory for the structure.
     scommand new_sc = malloc(sizeof(struct scommand_s));
     new_sc->sc_arguments = g_queue_new();
     new_sc->sc_file_input = NULL;
@@ -54,6 +55,7 @@ scommand scommand_destroy(scommand self){
 void scommand_push_back(scommand self, char * argument){
     assert(self != NULL && argument != NULL);
 
+    // Push the argument to the end of the queue.
     g_queue_push_tail(self->sc_arguments, argument);
 
     assert(!scommand_is_empty(self));
@@ -62,7 +64,10 @@ void scommand_push_back(scommand self, char * argument){
 void scommand_pop_front(scommand self){
     assert(self != NULL && !scommand_is_empty(self));
 
+    // removed_argument points to the first element in the queue.
     char * removed_argument = g_queue_pop_head(self->sc_arguments);
+
+    // Removing memory allocated for removed_argument since it's a useless variable.
     free(removed_argument);
     removed_argument = NULL;
 }
@@ -70,12 +75,14 @@ void scommand_pop_front(scommand self){
 void scommand_set_redir_in(scommand self, char * filename){
     assert(self != NULL);
 
+    // Set input dir.
     self->sc_file_input = filename;
 }
 
 void scommand_set_redir_out(scommand self, char * filename){
     assert(self != NULL);
 
+    // Set output dir.
     self->sc_file_output = filename;
 }
 
@@ -88,19 +95,20 @@ bool scommand_is_empty(const scommand self){
 unsigned int scommand_length(const scommand self){
     assert(self != NULL);
 
-    unsigned int sc_length = g_queue_get_length(self->sc_arguments);
+    unsigned int sc_args_length = g_queue_get_length(self->sc_arguments);
 
-    assert((sc_length == 0) == scommand_is_empty(self));
-    return sc_length;
+    assert((sc_args_length == 0) == scommand_is_empty(self));
+    return sc_args_length;
 }
 
 char * scommand_front(const scommand self){
     assert(self != NULL && !scommand_is_empty(self));
 
-    char * sc_front = g_queue_peek_head(self->sc_arguments);
+    // head element is stored in sc_front.
+    char * sc_arg_front = g_queue_peek_head(self->sc_arguments);
 
-    assert(sc_front != NULL);
-    return sc_front;
+    assert(sc_arg_front != NULL);
+    return sc_arg_front;
 }
 
 char * scommand_get_redir_in(const scommand self){
@@ -120,13 +128,16 @@ char * scommand_get_redir_out(const scommand self){
 char * scommand_to_string(const scommand self){
     assert(self != NULL);
 
+    // sc_shell_representation is the output of scommand_to_string.
+    // Not NULL since we can't concatenate a string with NULL.
     char * sc_shell_representation = "";
 
-    unsigned int sc_length = g_queue_get_length(self->sc_arguments);
-    if(sc_length != 0){
+    unsigned int sc_args_length = g_queue_get_length(self->sc_arguments);
+    if(sc_args_length != 0){
         sc_shell_representation = g_queue_peek_head(self->sc_arguments);
 
-        for(unsigned int i = 1; i < sc_length; i++){
+        // auxiliar_to_remove is used for the deletion of allocated memory blocks in previous strmerge calls.
+        for(unsigned int i = 1; i < sc_args_length; i++){
             char * auxiliar_to_remove = sc_shell_representation;
             sc_shell_representation = strmerge(sc_shell_representation," ");
             if(i != 1){
@@ -140,6 +151,7 @@ char * scommand_to_string(const scommand self){
         }
     }
 
+    // Add argument in the in redir field.
     if(self->sc_file_input != NULL){
         char * auxiliar_to_remove = sc_shell_representation;
         sc_shell_representation = strmerge(sc_shell_representation," < ");
@@ -150,6 +162,7 @@ char * scommand_to_string(const scommand self){
         free(auxiliar_to_remove);
     }
 
+    // Add argument in the out redir field.
     if(self->sc_file_output != NULL){
         char * auxiliar_to_remove = sc_shell_representation;
         sc_shell_representation = strmerge(sc_shell_representation," > ");
--- scommand testings/scommand/scommand_test.c
+++ scommand testings/scommand/scommand_test.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <assert.h>
+#include <string.h>
 #include <glib.h>
 #include "../../command.h"
 
@@ -12,36 +13,29 @@
 // - To view memory leaks and all heaps information
 //      gcc scommand_test.c ../../command.c ../../strextra.c -o ejecutable_scommand_test `pkg-config --cflags --libs glib-2.0` && valgrind --leak-check=full -v --track-origins=yes ./ejecutable_scommand_test < scommand_test_input.in
 
+#define SIZE 28
+
 int main(void){
-    char * a = malloc(sizeof(char*));
-    char * b = malloc(sizeof(char*));
-    char * c = malloc(sizeof(char*));
-    char * d = malloc(sizeof(char*));
-
     scommand test = scommand_new();
-    scanf("%s %s",&(*a),&(*b));
-    scommand_push_back(test,a);
-    scommand_push_back(test,b);
-    scanf("%s %s",&(*c),&(*d));
-    scommand_set_redir_in(test,c);
-    scommand_set_redir_out(test,d);
 
-    // char * representation = scommand_to_string(test);
-    // printf("%s\n",representation);
-    // free(representation);
-    // representation = NULL;
+    char * a[SIZE];
+    for(unsigned int i=0;i<SIZE;i++){
+        a[i]=malloc(sizeof(char)*1024);
+        scanf("%s",&(*a[i]));
+        if(i < 26){
+            scommand_push_back(test,a[i]);
+        }else if(i == 26){
+            scommand_set_redir_in(test,a[i]);
+        }else if(i == 27){
+            scommand_set_redir_out(test,a[i]);
+        }
+    }
 
-    // printf("%s\n",scommand_front(test));
-    // scommand_pop_front(test);
-    // printf("%s\n",scommand_front(test));
-    // scommand_pop_front(test);
-    // printf("%u\n",scommand_length(test));
-    // printf("%u\n",(unsigned int)scommand_is_empty(test));
-    // printf("%s\n",scommand_get_redir_in(test));
-    // printf("%s\n",scommand_get_redir_out(test));
+    char * representation = scommand_to_string(test);
+    printf("%s\n",representation);
+    free(representation);
+    representation = NULL;
 
     test = scommand_destroy(test);
-    printf("%u\n",test == NULL);
-    printf("%u\n",a == NULL);
     return 0;
 }
