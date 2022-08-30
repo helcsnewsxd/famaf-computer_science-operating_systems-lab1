3acc059cb540017ba1f8c52a1f9437dc8ec4ddfb
Lautaro Bachmann <laurigbach@gmail.com>
2022-08-30 20:19:48 -0300

    Solved merge conflict and added missing semicolon

--- command.c
+++ command.c
@@ -15,271 +15,256 @@
 // ----------- STRUCTURE --------------
 
 struct scommand_s {
-  GQueue* sc_arguments;
-  char* sc_file_input;
-  char* sc_file_output;
+    GQueue *sc_arguments;
+    char *sc_file_input;
+    char *sc_file_output;
 };
 
 // ----------- FUNCTIONS --------------
 
-scommand scommand_new(void)
-{
-  // Allocating memory for the structure.
-  scommand new_sc = malloc(sizeof(struct scommand_s));
-  new_sc->sc_arguments = g_queue_new();
-  new_sc->sc_file_input = NULL;
-  new_sc->sc_file_output = NULL;
+scommand scommand_new(void) {
+    // Allocating memory for the structure.
+    scommand new_sc = malloc(sizeof(struct scommand_s));
+    new_sc->sc_arguments = g_queue_new();
+    new_sc->sc_file_input = NULL;
+    new_sc->sc_file_output = NULL;
 
-  assert(new_sc != NULL && scommand_is_empty(new_sc) && scommand_get_redir_in(new_sc) == NULL && scommand_get_redir_out(new_sc) == NULL);
-  return new_sc;
+    assert(new_sc != NULL && scommand_is_empty(new_sc) && scommand_get_redir_in(new_sc) == NULL
+           && scommand_get_redir_out(new_sc) == NULL);
+    return new_sc;
 }
 
-scommand scommand_destroy(scommand self)
-{
-  assert(self != NULL);
+scommand scommand_destroy(scommand self) {
+    assert(self != NULL);
 
-  g_queue_free_full(self->sc_arguments, free);
-  if (self->sc_file_input != NULL) {
-    free(self->sc_file_input);
-    self->sc_file_input = NULL;
-  }
-  if (self->sc_file_output != NULL) {
-    free(self->sc_file_output);
-    self->sc_file_output = NULL;
-  }
-  free(self);
-  self = NULL;
+    g_queue_free_full(self->sc_arguments, free);
+    if (self->sc_file_input != NULL) {
+        free(self->sc_file_input);
+        self->sc_file_input = NULL;
+    }
+    if (self->sc_file_output != NULL) {
+        free(self->sc_file_output);
+        self->sc_file_output = NULL;
+    }
+    free(self);
+    self = NULL;
 
-  assert(self == NULL);
-  return self;
+    assert(self == NULL);
+    return self;
 }
 
-void scommand_push_back(scommand self, char* argument)
-{
-  assert(self != NULL && argument != NULL);
+void scommand_push_back(scommand self, char *argument) {
+    assert(self != NULL && argument != NULL);
 
-  // Push the argument to the end of the queue.
-  g_queue_push_tail(self->sc_arguments, argument);
+    // Push the argument to the end of the queue.
+    g_queue_push_tail(self->sc_arguments, argument);
 
-  assert(!scommand_is_empty(self));
+    assert(!scommand_is_empty(self));
 }
 
-void scommand_pop_front(scommand self)
-{
-  assert(self != NULL && !scommand_is_empty(self));
+void scommand_pop_front(scommand self) {
+    assert(self != NULL && !scommand_is_empty(self));
 
-  // removed_argument points to the first element in the queue.
-  char* removed_argument = g_queue_pop_head(self->sc_arguments);
+    // removed_argument points to the first element in the queue.
+    char *removed_argument = g_queue_pop_head(self->sc_arguments);
 
-  // Removing memory allocated for removed_argument since it's a useless variable.
-  free(removed_argument);
-  removed_argument = NULL;
+    // Removing memory allocated for removed_argument since it's a useless variable.
+    free(removed_argument);
+    removed_argument = NULL;
 }
 
-void scommand_set_redir_in(scommand self, char* filename)
-{
-  assert(self != NULL);
+void scommand_set_redir_in(scommand self, char *filename) {
+    assert(self != NULL);
 
-  // Set input dir.
-  self->sc_file_input = filename;
+    // Set input dir.
+    self->sc_file_input = filename;
 }
 
-void scommand_set_redir_out(scommand self, char* filename)
-{
-  assert(self != NULL);
+void scommand_set_redir_out(scommand self, char *filename) {
+    assert(self != NULL);
 
-  // Set output dir.
-  self->sc_file_output = filename;
+    // Set output dir.
+    self->sc_file_output = filename;
 }
 
-bool scommand_is_empty(const scommand self)
-{
-  assert(self != NULL);
+bool scommand_is_empty(const scommand self) {
+    assert(self != NULL);
 
-  return g_queue_is_empty(self->sc_arguments);
+    return g_queue_is_empty(self->sc_arguments);
 }
 
-unsigned int scommand_length(const scommand self)
-{
-  assert(self != NULL);
+unsigned int scommand_length(const scommand self) {
+    assert(self != NULL);
 
-  unsigned int sc_args_length = g_queue_get_length(self->sc_arguments);
+    unsigned int sc_args_length = g_queue_get_length(self->sc_arguments);
 
-  assert((sc_args_length == 0) == scommand_is_empty(self));
-  return sc_args_length;
+    assert((sc_args_length == 0) == scommand_is_empty(self));
+    return sc_args_length;
 }
 
-char* scommand_front(const scommand self)
-{
-  assert(self != NULL && !scommand_is_empty(self));
+char *scommand_front(const scommand self) {
+    assert(self != NULL && !scommand_is_empty(self));
 
-  // head element is stored in sc_front.
-  char* sc_arg_front = g_queue_peek_head(self->sc_arguments);
+    // head element is stored in sc_front.
+    char *sc_arg_front = g_queue_peek_head(self->sc_arguments);
 
-  assert(sc_arg_front != NULL);
-  return sc_arg_front;
+    assert(sc_arg_front != NULL);
+    return sc_arg_front;
 }
 
-char* scommand_get_redir_in(const scommand self)
-{
-  assert(self != NULL);
+char *scommand_get_redir_in(const scommand self) {
+    assert(self != NULL);
 
-  return self->sc_file_input;
+    return self->sc_file_input;
 }
 
-char* scommand_get_redir_out(const scommand self)
-{
-  assert(self != NULL);
+char *scommand_get_redir_out(const scommand self) {
+    assert(self != NULL);
 
-  return self->sc_file_output;
+    return self->sc_file_output;
 }
 
 // ----------- FUNCTION TO DEBUG --------------
 
-char* scommand_to_string(const scommand self)
-{
-  assert(self != NULL);
+char *scommand_to_string(const scommand self) {
+    assert(self != NULL);
 
-  // sc_shell_representation is the output of scommand_to_string.
-  // Not NULL since we can't concatenate a string with NULL.
-  char* sc_shell_representation = "";
+    // sc_shell_representation is the output of scommand_to_string.
+    // Not NULL since we can't concatenate a string with NULL.
+    char *sc_shell_representation = "";
 
-  unsigned int sc_args_length = g_queue_get_length(self->sc_arguments);
-  if (sc_args_length != 0) {
-    sc_shell_representation = g_queue_peek_head(self->sc_arguments);
+    unsigned int sc_args_length = g_queue_get_length(self->sc_arguments);
+    if (sc_args_length != 0) {
+        sc_shell_representation = g_queue_peek_head(self->sc_arguments);
 
-    // auxiliar_to_remove is used for the deletion of allocated memory blocks in previous strmerge calls.
-    for (unsigned int i = 1; i < sc_args_length; i++) {
-      char* auxiliar_to_remove = sc_shell_representation;
-      sc_shell_representation = strmerge(sc_shell_representation, " ");
-      if (i != 1) {
-	free(auxiliar_to_remove);
-      }
+        // auxiliar_to_remove is used for the deletion of allocated memory blocks in previous
+        // strmerge calls.
+        for (unsigned int i = 1; i < sc_args_length; i++) {
+            char *auxiliar_to_remove = sc_shell_representation;
+            sc_shell_representation = strmerge(sc_shell_representation, " ");
+            if (i != 1) {
+                free(auxiliar_to_remove);
+            }
 
-      auxiliar_to_remove = sc_shell_representation;
-      sc_shell_representation = strmerge(sc_shell_representation, g_queue_peek_nth(self->sc_arguments, i));
-      free(auxiliar_to_remove);
+            auxiliar_to_remove = sc_shell_representation;
+            sc_shell_representation =
+                strmerge(sc_shell_representation, g_queue_peek_nth(self->sc_arguments, i));
+            free(auxiliar_to_remove);
+        }
     }
-  }
 
-  // Add argument in the in redir field.
-  if (self->sc_file_input != NULL) {
-    char* auxiliar_to_remove = sc_shell_representation;
-    sc_shell_representation = strmerge(sc_shell_representation, " < ");
-    free(auxiliar_to_remove);
+    // Add argument in the in redir field.
+    if (self->sc_file_input != NULL) {
+        char *auxiliar_to_remove = sc_shell_representation;
+        sc_shell_representation = strmerge(sc_shell_representation, " < ");
+        free(auxiliar_to_remove);
 
-    auxiliar_to_remove = sc_shell_representation;
-    sc_shell_representation = strmerge(sc_shell_representation, self->sc_file_input);
-    free(auxiliar_to_remove);
-  }
+        auxiliar_to_remove = sc_shell_representation;
+        sc_shell_representation = strmerge(sc_shell_representation, self->sc_file_input);
+        free(auxiliar_to_remove);
+    }
 
-  // Add argument in the out redir field.
-  if (self->sc_file_output != NULL) {
-    char* auxiliar_to_remove = sc_shell_representation;
-    sc_shell_representation = strmerge(sc_shell_representation, " > ");
-    free(auxiliar_to_remove);
+    // Add argument in the out redir field.
+    if (self->sc_file_output != NULL) {
+        char *auxiliar_to_remove = sc_shell_representation;
+        sc_shell_representation = strmerge(sc_shell_representation, " > ");
+        free(auxiliar_to_remove);
 
-    auxiliar_to_remove = sc_shell_representation;
-    sc_shell_representation = strmerge(sc_shell_representation, self->sc_file_output);
-    free(auxiliar_to_remove);
-  }
+        auxiliar_to_remove = sc_shell_representation;
+        sc_shell_representation = strmerge(sc_shell_representation, self->sc_file_output);
+        free(auxiliar_to_remove);
+    }
 
-  assert(scommand_get_redir_in(self) == NULL || scommand_get_redir_out(self) == NULL || strlen(sc_shell_representation) > 0);
-  return sc_shell_representation;
+    assert(scommand_get_redir_in(self) == NULL || scommand_get_redir_out(self) == NULL
+           || strlen(sc_shell_representation) > 0);
+    return sc_shell_representation;
 }
 
 struct pipeline_s {
-  GQueue* commands;
-  bool should_wait;
+    GQueue *commands;
+    bool should_wait;
 } pipeline_s;
 
-pipeline pipeline_new(void)
-{
-  pipeline self;
-  self = malloc(sizeof(pipeline_s));
-  self->commands = NULL;
-  self->should_wait = true;
+pipeline pipeline_new(void) {
+    pipeline self;
+    self = malloc(sizeof(pipeline_s));
+    self->commands = NULL;
+    self->should_wait = true;
 
-  // Didn't put predicate members inside variables to prevent useless computation when deactivating assertions
-  assert(self != NULL && pipeline_is_empty(self) && pipeline_get_wait(self));
-  return self;
+    // Didn't put predicate members inside variables to prevent useless computation when
+    // deactivating assertions
+    assert(self != NULL && pipeline_is_empty(self) && pipeline_get_wait(self));
+    return self;
 }
 
-pipeline pipeline_destroy(pipeline self)
-{
-  assert(self != NULL);
-  g_queue_free_full(self->commands, free); // free queue using second arg to destroy each element
-  self->commands = NULL;
-  free(self);
-  self = NULL;
-  assert(self == NULL);
-  return self;
+pipeline pipeline_destroy(pipeline self) {
+    assert(self != NULL);
+    g_queue_free_full(self->commands, free); // free queue using second arg to destroy each element
+    self->commands = NULL;
+    free(self);
+    self = NULL;
+    assert(self == NULL);
+    return self;
 }
 
-void pipeline_push_back(pipeline self, scommand sc)
-{
-  assert(self != NULL && sc != NULL);
-  g_queue_push_tail(self->commands, sc); // Send sc to end of Queue
-  assert(!pipeline_is_empty(self));
+void pipeline_push_back(pipeline self, scommand sc) {
+    assert(self != NULL && sc != NULL);
+    g_queue_push_tail(self->commands, sc); // Send sc to end of Queue
+    assert(!pipeline_is_empty(self));
 }
 
-void pipeline_pop_front(pipeline self)
-{
-  assert(self != NULL && !pipeline_is_empty(self));
-  g_queue_pop_head(self->commands); // Remove first element from Queue
+void pipeline_pop_front(pipeline self) {
+    assert(self != NULL && !pipeline_is_empty(self));
+    g_queue_pop_head(self->commands); // Remove first element from Queue
 }
 
-void pipeline_set_wait(pipeline self, const bool w)
-{
-  assert(self != NULL);
-  self->should_wait = w;
+void pipeline_set_wait(pipeline self, const bool w) {
+    assert(self != NULL);
+    self->should_wait = w;
 }
 
-bool pipeline_is_empty(const pipeline self)
-{
-  assert(self != NULL);
-  return g_queue_is_empty(self->commands);
+bool pipeline_is_empty(const pipeline self) {
+    assert(self != NULL);
+    return g_queue_is_empty(self->commands);
 }
 
-unsigned int pipeline_length(const pipeline self)
-{
-  assert(self != NULL);
-  unsigned int result;
-  result = g_queue_get_length(self->commands);
-  assert((result == 0) == pipeline_is_empty(self));
-  return result;
+unsigned int pipeline_length(const pipeline self) {
+    assert(self != NULL);
+    unsigned int result;
+    result = g_queue_get_length(self->commands);
+    assert((result == 0) == pipeline_is_empty(self));
+    return result;
 }
 
-scommand pipeline_front(const pipeline self)
-{
-  assert(self != NULL && !pipeline_is_empty(self));
-  scommand result;
-  result = g_queue_peek_head(self->commands); // Returns the first element of the queue.
-  assert(result != NULL);
-  return result;
+scommand pipeline_front(const pipeline self) {
+    assert(self != NULL && !pipeline_is_empty(self));
+    scommand result;
+    result = g_queue_peek_head(self->commands); // Returns the first element of the queue.
+    assert(result != NULL);
+    return result;
 }
 
-bool pipeline_get_wait(const pipeline self)
-{
-  assert(self != NULL);
-  return self->should_wait;
+bool pipeline_get_wait(const pipeline self) {
+    assert(self != NULL);
+    return self->should_wait;
 }
 
-char* pipeline_to_string(const pipeline self)
-{
-  assert(self != NULL);
-  char* result;
-  unsigned int length_of_pipeline;
-  bool is_last_elem = false;
-  result = "";
-  length_of_pipeline = pipeline_length(self)
-  for (unsigned int i = 0u; i < length_of_pipeline; ++i) { // 
-    strmerge(result, g_queue_peek_nth(self->commands, i)); // g_queue_peek_nth returns the n'th element of queue.
-    is_last_elem = i < length_of_pipeline - 1; 
-    if (! is_last_elem) { // Check if it is not the last element to add " | " between the this command string and the following command string
-      strmerge(result, " | ");
+char *pipeline_to_string(const pipeline self) {
+    assert(self != NULL);
+    char *result;
+    unsigned int length_of_pipeline;
+    bool is_last_elem = false;
+    result = "";
+    length_of_pipeline = pipeline_length(self);
+    for (unsigned int i = 0u; i < length_of_pipeline; ++i) { //
+        strmerge(result,
+                 g_queue_peek_nth(self->commands,
+                                  i)); // g_queue_peek_nth returns the n'th element of queue.
+        is_last_elem = i < length_of_pipeline - 1;
+        if (!is_last_elem) { // Check if it is not the last element to add " | " between the this
+                             // command string and the following command string
+            strmerge(result, " | ");
+        }
+        assert(pipeline_is_empty(self) || pipeline_get_wait(self) || strlen(result) > 0);
+        return result;
     }
-  }
-  assert(pipeline_is_empty(self) || pipeline_get_wait(self) || strlen(result) > 0);
-  return result;
-}
