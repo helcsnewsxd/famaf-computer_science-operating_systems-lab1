c8658fc066c5574e6763a4769879363cd1f92ccb
EmanuelHerrador <emanuel.nicolas.herrador@unc.edu.ar>
2022-09-06 20:11:33 -0300

    Parsing functions implemented with 0 errors and 0 failures. Also, we add a function in strextra that check if the string has any character different to spaces ('/n',' ','/t',etc.)
    Co-authored-by: Juan Bratti <juanbratti@mi.unc.edu.ar>

--- parsing.c
+++ parsing.c
@@ -1,40 +1,91 @@
 #include <stdlib.h>
 #include <stdbool.h>
+#include <check.h>
 
 #include "parsing.h"
 #include "parser.h"
 #include "command.h"
+#include "strextra.h"
 
 static scommand parse_scommand(Parser p) {
-    /* Devuelve NULL cuando hay un error de parseo */
-    return NULL;
+    scommand new_scmd = scommand_new();
+
+    arg_kind_t arg_type = ARG_INPUT;
+    char * arg_cmd = parser_next_argument(p,&arg_type);
+    
+    while(arg_cmd != NULL){
+        // We've the cmd argument in *arg_cmd
+
+        if(arg_type == ARG_NORMAL){
+            scommand_push_back(new_scmd,arg_cmd);
+        }else if(arg_type == ARG_INPUT){
+            scommand_set_redir_in(new_scmd,arg_cmd);
+        }else if(arg_type == ARG_OUTPUT){
+            scommand_set_redir_out(new_scmd,arg_cmd);
+        }
+
+        // New argument
+        arg_cmd = NULL;
+        arg_cmd = parser_next_argument(p,&arg_type);
+    }
+
+    // See if there are some errors
+    if(scommand_is_empty(new_scmd)){
+        new_scmd = scommand_destroy(new_scmd);
+        new_scmd = NULL;
+    }
+
+    return new_scmd;
 }
 
 pipeline parse_pipeline(Parser p) {
     pipeline result = pipeline_new();
-    scommand cmd = NULL;
-    bool error = false, another_pipe=true;
+    scommand act_scmd = NULL;
+    bool error = false, another_pipe = true;
 
-    cmd = parse_scommand(p);
-    error = (cmd==NULL); /* Comando inválido al empezar */
+    act_scmd = parse_scommand(p);
+    error = (act_scmd == NULL); // Invalid command to begin
+
     while (another_pipe && !error) {
-        /*
-         * COMPLETAR
-         *
-         */
-    }
-    /* Opcionalmente un OP_BACKGROUND al final */
-    }
-    /*
-     *
-     * COMPLETAR
-     *
-     */
+        // Push into a pipeline
+        pipeline_push_back(result,act_scmd);
 
-    /* Tolerancia a espacios posteriores */
-    /* Consumir todo lo que hay inclusive el \n */
-    /* Si hubo error, hacemos cleanup */
+        // See final options (end line, pipe, background or EOF)
+        // Is there a pipe?
+        parser_op_pipe(p,&another_pipe);
 
-    return NULL; // MODIFICAR
+        if(another_pipe){
+            // Parsing the next scommand
+            act_scmd = parse_scommand(p);
+
+            error = (act_scmd == NULL); // Invalid command
+        }
+    }
+
+    if(!error){
+        // Is there a background?
+        bool there_is_background = false;
+        parser_op_background(p,&there_is_background);
+        pipeline_set_wait(result,!there_is_background);
+        
+        // Erase the rest of the parser
+        bool there_are_other_symbols = false;
+        parser_garbage(p,&there_are_other_symbols);
+        // Check if we've an error of command syntax
+        char * forgotten_symbols = parser_last_garbage(p);
+        error = (forgotten_symbols != NULL && string_has_a_not_space(forgotten_symbols));
+    }else{
+        // Erase the rest of the parser
+        bool there_are_other_symbols = false;
+        parser_garbage(p,&there_are_other_symbols);
+    }
+
+    // Erase the pipeline in case of an error
+    if(error){
+        result = pipeline_destroy(result);
+        result = NULL;
+    }
+
+    // Return the pipeline or NULL (in case of a syntax error)
+    return result;
 }
-
