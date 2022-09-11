e3118cc75acd65409e2be9de58860cde494c4d44
Lautaro Bachmann <laurigbach@gmail.com>
2022-09-11 12:33:08 -0300

    Created makefile and helper function to convert from string to pipeline

---
+++ custom_tests/execute/execute_test.c
@@ -0,0 +1,41 @@
+#include "../../execute.h"
+#include "../../parser.h"
+#include "../../parsing.h"
+#include <assert.h>
+#include <glib.h>
+#include <stdio.h>
+#include <string.h>
+
+// helper function to create pipeline from string
+static pipeline get_pipeline_from_string(char *input_str) {
+    pipeline pipe = NULL;
+    FILE *temp = tmpfile();
+    if (temp) {
+        fprintf(temp, "%s", input_str);
+    }
+    rewind(temp);
+
+    Parser input = parser_new(temp);
+
+    pipe = parse_pipeline(input);
+    fclose(temp);
+    return pipe;
+}
+
+// static void test_simple_command(char *input) {
+//     pipeline pipe = get_pipeline_from_string(input);
+//     execute_pipeline(pipe);
+// }
+static void test_helper(char *cmd) {
+    pipeline pipe1 = get_pipeline_from_string(strdup(cmd));
+    char *pipe_str = pipeline_to_string(pipe1);
+    g_assert_cmpstr(pipe_str, ==, cmd);
+}
+
+int main(void) {
+    // test if helper function works
+    test_helper("ls -l");
+
+    printf("All tests passed!\n");
+    return 0;
+}
