a29face76b23d35806e8ca7536893667436d6755
EmanuelHerrador <emanuel.nicolas.herrador@unc.edu.ar>
2022-09-13 16:58:24 -0300

    Added change file descriptor's functions Worked with JuanBratti

---
+++ files_descriptors.c
@@ -0,0 +1,46 @@
+#include <assert.h>
+#include <fcntl.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#include "files_descriptors.h"
+
+void change_file_descriptor_input(char *redir_in) {
+    if (redir_in != NULL) {
+        // We've to change the reader's file descriptor
+        int fd_act = open(redir_in, O_RDONLY);
+        SYS_ERROR(fd_act == -1, "OpenError");
+
+        int syscall_result = dup2(fd_act, STDIN_FILENO);
+        SYS_ERROR(syscall_result == -1, "Dup2Error");
+
+        syscall_result = close(fd_act);
+        SYS_ERROR(syscall_result == -1, "CloseError");
+    }
+}
+
+void change_file_descriptor_output(char *redir_out) {
+    if (redir_out != NULL) {
+        // We've to change the writer's file descriptor
+        int fd_act = open(redir_out, O_WRONLY | O_CREAT, S_IRUSR | S_IWUSR);
+        SYS_ERROR(fd_act == -1, "OpenError");
+
+        int syscall_result = dup2(fd_act, STDOUT_FILENO);
+        SYS_ERROR(syscall_result == -1, "Dup2Error");
+
+        syscall_result = close(fd_act);
+        SYS_ERROR(syscall_result == -1, "CloseError");
+    }
+}
+
+void change_file_descriptor_input_from_fd(int fd_act) {
+    int syscall_result = dup2(fd_act, STDIN_FILENO);
+    SYS_ERROR(syscall_result == -1, "Dup2Error");
+}
+
+void change_file_descriptor_output_from_fd(int fd_act) {
+    int syscall_result = dup2(fd_act, STDOUT_FILENO);
+    SYS_ERROR(syscall_result == -1, "Dup2Error");
+}
---
+++ files_descriptors.h
@@ -0,0 +1,41 @@
+#ifndef FILE_DESCRIPTORS
+#define FILE_DESCRIPTORS
+
+#include <stdio.h>
+
+#define SYS_ERROR(condition, message) if(condition){perror(message);exit(EXIT_FAILURE);}
+/**
+ * @brief Define used to report syscall and exit errors if needed
+ * 
+ */
+
+
+void change_file_descriptor_input(char *redir_in);
+/**
+ * @brief Change the actual INPUT of the process to the file's file descriptor
+ * 
+ */
+
+
+void change_file_descriptor_output(char *redir_out);
+/**
+ * @brief Change the actual OUTPUT of the process to the file's file descriptor
+ * 
+ */
+
+
+void change_file_descriptor_input_from_fd(int fd_act);
+/**
+ * @brief Change the actual INPUT of the process to the file descriptor
+ * 
+ */
+
+
+void change_file_descriptor_output_from_fd(int fd_act);
+/**
+ * @brief Change the actual OUTPUT of the process to the file descriptor
+ * 
+ */
+
+
+#endif
