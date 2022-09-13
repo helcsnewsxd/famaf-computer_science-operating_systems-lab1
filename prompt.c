d5249d3536288a50d6d75b70f1119d092fbdfbbe
EmanuelHerrador <emanuel.nicolas.herrador@unc.edu.ar>
2022-09-13 17:03:50 -0300

    Add help prompt (using colors) Worked with JuanBratti

--- prompt.c
+++ prompt.c
@@ -5,9 +5,19 @@
 
 // COLORS
 #define ANSI_COLOR_RED     "\033[0;31m"
+#define ANSI_COLOR_RED2     "\033[1;31m"
 #define ANSI_COLOR_GREEN   "\x1b[1;32m"
 #define ANSI_COLOR_BLUE    "\033[1;34m"
 #define ANSI_COLOR_RESET   "\x1b[0m"
+#define BLK "\033[0;30m"
+#define GRN "\033[0;32m"
+#define YEL "\033[0;33m"
+#define MAG "\033[0;35m"
+#define CYN "\033[0;36m"
+#define WHT "\033[1;37m"
+
+// UNDERLINED
+#define UWTH "\033[4;37m"
 
 #include "prompt.h"
 #include "strextra.h"
@@ -35,4 +45,28 @@ void show_prompt(void) {
     printf(ANSI_COLOR_RESET);
 
     fflush(stdout);
+}
+
+void prompt_help(void) {
+    printf("\n===================================================================\n");
+    printf(ANSI_COLOR_RED2 "\n(MyBash) -> A bash scripting project\n" ANSI_COLOR_RED2);
+    printf(ANSI_COLOR_RESET);
+    printf("Proyecto para la materia Sistemas Operativos 2022, FAMAF UNC\n\n");
+    printf(WHT "Integrantes:\n" WHT);
+    printf(ANSI_COLOR_RESET);
+    printf(" - Lautaro Bachmann\n - Juan Bratti\n - Gonzalo Canavesio\n - Emanuel Herrador\n");
+    printf(WHT "\nComandos Internos Implementados:\n" WHT);
+    printf(ANSI_COLOR_RESET);
+    printf(
+    " ‣ Comando 'cd':   Permite moverse entre directorios del sistema\n"
+    "                   Modo de uso: cd {ruta/_absoluta/_o/_relativa}\n"
+    " ‣ Comando 'exit': Finaliza la ejecucion de MyBash y los procesos asociados no backgrounds\n"
+    "                   Modo de uso: exit\n"
+    " ‣ Comando 'help': Envia un mensaje por salida estandar indicando:\n"
+    "                       - Nombre de la shell\n"
+    "                       - Nombre de sus autores\n"
+    "                       - Lista de comandos internos implementados con breve descripcion\n"
+    "                   Modo de uso: help\n"
+    );
+    printf("\n===================================================================\n");
 }
--- prompt.h
+++ prompt.h
@@ -4,7 +4,14 @@
 void show_prompt(void);
 /**
  * @brief This is a function to do a prompt of my bash
- * 
+ *
+ */
+
+void prompt_help(void);
+/**
+ * @brief This is a function to do a help promp in my bash when the command
+ * executing is "help"
+ *
  */
 
 #endif
