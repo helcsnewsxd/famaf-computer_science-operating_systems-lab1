0fde66e125e17ec793f4f13f1fb86ed09c0f20a1
EmanuelHerrador <emanuel.nicolas.herrador@unc.edu.ar>
2022-09-13 17:06:01 -0300

    Assert requires added Worked with JuanBratti

--- parsing.c
+++ parsing.c
@@ -1,35 +1,38 @@
-#include <stdlib.h>
-#include <stdbool.h>
+#include <assert.h>
 #include <check.h>
+#include <stdbool.h>
+#include <stdlib.h>
 
-#include "parsing.h"
-#include "parser.h"
 #include "command.h"
+#include "parser.h"
+#include "parsing.h"
 
 static scommand parse_scommand(Parser p) {
+    assert(p != NULL);
+
     scommand new_scmd = scommand_new();
 
     arg_kind_t arg_type = ARG_INPUT;
-    char * arg_cmd = parser_next_argument(p,&arg_type);
-    
-    while(arg_cmd != NULL){
+    char *arg_cmd = parser_next_argument(p, &arg_type);
+
+    while (arg_cmd != NULL) {
         // We've the cmd argument in *arg_cmd
 
-        if(arg_type == ARG_NORMAL){
-            scommand_push_back(new_scmd,arg_cmd);
-        }else if(arg_type == ARG_INPUT){
-            scommand_set_redir_in(new_scmd,arg_cmd);
-        }else if(arg_type == ARG_OUTPUT){
-            scommand_set_redir_out(new_scmd,arg_cmd);
+        if (arg_type == ARG_NORMAL) {
+            scommand_push_back(new_scmd, arg_cmd);
+        } else if (arg_type == ARG_INPUT) {
+            scommand_set_redir_in(new_scmd, arg_cmd);
+        } else if (arg_type == ARG_OUTPUT) {
+            scommand_set_redir_out(new_scmd, arg_cmd);
         }
 
         // New argument
         arg_cmd = NULL;
-        arg_cmd = parser_next_argument(p,&arg_type);
+        arg_cmd = parser_next_argument(p, &arg_type);
     }
 
     // See if there are some errors
-    if(scommand_is_empty(new_scmd)){
+    if (scommand_is_empty(new_scmd)) {
         new_scmd = scommand_destroy(new_scmd);
         new_scmd = NULL;
     }
@@ -38,6 +41,8 @@ static scommand parse_scommand(Parser p) {
 }
 
 pipeline parse_pipeline(Parser p) {
+    assert(p != NULL && !parser_at_eof(p));
+
     pipeline result = pipeline_new();
     scommand act_scmd = NULL;
     bool error = false, another_pipe = true;
@@ -47,13 +52,13 @@ pipeline parse_pipeline(Parser p) {
 
     while (another_pipe && !error) {
         // Push into a pipeline
-        pipeline_push_back(result,act_scmd);
+        pipeline_push_back(result, act_scmd);
 
         // See final options (end line, pipe, background or EOF)
         // Is there a pipe?
-        parser_op_pipe(p,&another_pipe);
+        parser_op_pipe(p, &another_pipe);
 
-        if(another_pipe){
+        if (another_pipe) {
             // Parsing the next scommand
             act_scmd = parse_scommand(p);
 
@@ -61,24 +66,24 @@ pipeline parse_pipeline(Parser p) {
         }
     }
 
-    if(!error){
+    if (!error) {
         // Is there a background?
         bool there_is_background = false;
-        parser_op_background(p,&there_is_background);
-        pipeline_set_wait(result,!there_is_background);
-        
+        parser_op_background(p, &there_is_background);
+        pipeline_set_wait(result, !there_is_background);
+
         // Erase the rest of the parser
         bool there_are_other_symbols = false;
-        parser_garbage(p,&there_are_other_symbols);
+        parser_garbage(p, &there_are_other_symbols);
         error = there_are_other_symbols;
-    }else{
+    } else {
         // Erase the rest of the parser
         bool there_are_other_symbols = false;
-        parser_garbage(p,&there_are_other_symbols);
+        parser_garbage(p, &there_are_other_symbols);
     }
 
     // Erase the pipeline in case of an error
-    if(error){
+    if (error) {
         result = pipeline_destroy(result);
         result = NULL;
     }
